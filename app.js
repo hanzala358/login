050e2a5a89db43e820d10853b8f6eb008a12d042
hanzalaa358 <hanzala358>
2024-08-09 13:29:31 +0500

    changing

--- app.js
+++ app.js
@@ -1,51 +1,50 @@
-// get data from signup
+
 const signup_form = document.querySelector("#signup_form");
 const signup_email = document.querySelector("#signup_email");
 const signup_password = document.querySelector("#signup_password");
 const User_name = document.querySelector("#User_name");
 
-// get data from login
+
 
 const login_form = document.querySelector("#login_form");
 const login_email = document.querySelector("#login_email");
 const login_password = document.querySelector("#login_password");
 const login_age = document.querySelector("#login_age")
-// for transition
+
 
 const chk = document.getElementById("chk");
 const login = document.querySelector(".login");
 const signup = document.querySelector(".signup");
 const loginLabel = document.querySelector(".login label");
 const signupLabel = document.querySelector(".signup label");
-// get data from localStorage
+
 const User_auth_data = JSON.parse(localStorage.getItem("User_auth_data"));
 
-// all data came in this Array
+
 let User_auth_arr;
 
 User_auth_data == null
   ? (User_auth_arr = [])
   : (User_auth_arr = User_auth_data);
 
-// alert position setting
+
 alertify.set("notifier", "position", "top-center");
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const passwordRegex = /^(?=.*[A-Z])(?=.*\W).{5,}$/;
 const usernameRegex = /^.{5,}$/;
 
-// signup authentication Response
 signup_form.addEventListener("submit", (e) => {
   e.preventDefault();
   let check_method = true;
 
-  // Validate email for signup
+  
   if (!emailRegex.test(signup_email.value)) {
     alertify.error("Invalid email format");
     return;
   }
 
-  // Validate password
+  
   if (!passwordRegex.test(signup_password.value)) {
     alertify.error(
       "Password must be at least 5 characters long, with special Character,Captial letter "
@@ -53,13 +52,13 @@ signup_form.addEventListener("submit", (e) => {
     return;
   }
 
-  // Validate username
+  
   if (!usernameRegex.test(User_name.value)) {
     alertify.error("Username must be at least 5 characters long");
     return;
   }
 
-  // getting true or false by checking email
+  
 
   check_method = User_auth_arr.some(
     (item) => item.email === signup_email.value
@@ -88,19 +87,19 @@ signup_form.addEventListener("submit", (e) => {
   }
 });
 
-// Login Authentication Response
+
 
 login_form.addEventListener("submit", (e) => {
   e.preventDefault();
-  // console.log('working');
-  // Validate email for Login
+  // console.log('work');
+  
 
   if (!emailRegex.test(login_email.value)) {
     alertify.error("Invalid email format");
     return;
   }
 
-  // filter email
+  
 
   let check_method = User_auth_arr.filter(
     (item) => item.email === login_email.value
